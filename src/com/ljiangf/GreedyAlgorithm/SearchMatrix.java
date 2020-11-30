6b9ccb1f5667caf9868ad1d4d7d192d4e4011eb8
ljf <570055398@qq.com>
2020-11-30 19:07:57 +0800

    add huffman tree

--- src/com/ljiangf/GreedyAlgorithm/SearchMatrix.java
+++ src/com/ljiangf/GreedyAlgorithm/SearchMatrix.java
@@ -1,3 +1,26 @@
 package com.ljiangf.GreedyAlgorithm;
+//https://leetcode-cn.com/problems/search-a-2d-matrix-ii/
+//tested correct on Leetcode
 public class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int col = 0;
+        int row = matrix.length-1;
+        while(row != -1){
+            if(matrix[row][col] > target){
+                --row;
+                if(row == -1){
+                    return false;
+                }
+            }else if(matrix[row][col] < target){
+                ++col;
+                if(col == matrix[0].length){
+                    --col;
+                    --row;
+                }
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
 }
