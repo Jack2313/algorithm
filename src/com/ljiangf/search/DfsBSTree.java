0a6d802cee897fd979505fa04782f18df80933b1
ljf <570055398@qq.com>
2020-12-16 17:57:58 +0800

    add bin search

---
+++ src/com/ljiangf/search/DfsBSTree.java
@@ -0,0 +1,2 @@
+package com.ljiangf.search;public class DfsBSTree {
+}
---
+++ src/com/ljiangf/search/DfsTree.java
@@ -0,0 +1,14 @@
+package com.ljiangf.search;
+
+public class DFS {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+
+    }
+}
